fb8cbf005c490a787738b49da886bb167be32ad0
Mahesh <maheshvp055@gmail.com>
2026-07-09 22:56:31 +0530

    Variables Practice

---
+++ 01_variables/02_variables.py
@@ -0,0 +1,19 @@
+name = "Mahesh"
+age = 32
+height = 170.5
+is_dba = True
+
+print(name)
+print(age)
+print(height)
+print(is_dba)
+
+print(type(name))
+print(type(age))
+print(type(height))
+print(type(is_dba))
+
+print(id(name))
+print(id(age))
+print(id(height))
+print(id(is_dba))
---
+++ 01_variables/02_variables_exercise.py
@@ -0,0 +1,15 @@
+name='Mahesh'
+current_company='HCLTech'
+exp=9
+city='Bangalore'
+salary=122000
+Know_SQL=True
+Know_Python=False
+
+print('Name: ',name)
+print('Company: ', current_company)
+print('Experience: ', exp)
+print('City: ', city)
+print('Salary: ', salary)
+print('Knows SQL: ', Know_SQL)
+print('Knows Python: ', Know_Python)
