7806c07cd2e0e11ce0bc0c5bca292de3d6a6fa92
Mahesh <maheshvp055@gmail.com>
2026-07-09 23:01:32 +0530

    Arithmetic Op on Variables

--- 01_variables/02_variables_exercise.py
+++ 01_variables/02_variables_exercise.py
@@ -1,15 +1,15 @@
 name='Mahesh'
 current_company='HCLTech'
-exp=9
+experience=9
 city='Bangalore'
 salary=122000
-Know_SQL=True
-Know_Python=False
+knows_sql=True
+knows_python=False
 
 print('Name: ',name)
 print('Company: ', current_company)
-print('Experience: ', exp)
+print('Experience: ', experience)
 print('City: ', city)
 print('Salary: ', salary)
-print('Knows SQL: ', Know_SQL)
-print('Knows Python: ', Know_Python)
+print('Knows SQL: ', knows_sql)
+print('Knows Python: ', knows_python)
---
+++ 01_variables/03_variable_update.py
@@ -0,0 +1,4 @@
+salary=122000
+new_salary=salary + 70000
+print('Current Salary: ', salary)
+print('New Salary: ', new_salary)
