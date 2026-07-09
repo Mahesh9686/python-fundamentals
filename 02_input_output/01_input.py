2c97a43adaca1d109553353721fddb90bc9d6173
Mahesh <maheshvp055@gmail.com>
2026-07-09 23:33:44 +0530

    Add input learning

---
+++ 02_input_output/01_input.py
@@ -0,0 +1,7 @@
+name=input('Enter your Name: ')
+print("Hello", name)
+
+age = int(input("Enter your age: "))
+
+print(age)
+print(type(age))
---
+++ 02_input_output/02_user_profile.py
@@ -0,0 +1,9 @@
+name=input('Enter your name:\n')
+company=input('Enter company name:\n')
+experience=input('How many years of experience do you have?\n')
+city=input('Which city do you live in:\n')
+
+print("Name: ", name)
+print("Company: ", company)
+print("Experience: ", experience)
+print("City: ", city)
