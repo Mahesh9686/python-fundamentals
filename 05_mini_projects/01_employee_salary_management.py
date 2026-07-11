47ce664f59c8369e05b4b10c081b3ebe0230fc88
Mahesh <maheshvp055@gmail.com>
2026-07-12 01:04:37 +0530

    Add loops

--- 05_mini_projects/01_employee_salary_management.py
+++ 05_mini_projects/01_employee_salary_management.py
@@ -1,51 +1,52 @@
 ########################################################
 # Input
 ########################################################
-employee_name=input("Enter Name of the Employee: ")
-employee_id=input("Enter Employee ID: ")
-department=input("Enter Department of the Employee: ")
-salary=float(input("Enter Salary of the Employee: "))
-experience=int(input("Enter Years of Experience: "))
-BONUS_EXPERIENCED=20
-BONUS_NEW=10
-HRA_PERCENT=20
+for emp in range(101,106):
+    employee_name=input("Enter Name of the Employee: ")
+    department=input("Enter Department of the Employee: ")
+    salary=float(input("Enter Salary of the Employee: "))
+    experience=int(input("Enter Years of Experience: "))
+    BONUS_EXPERIENCED=20
+    BONUS_NEW=10
+    HRA_PERCENT=20
+    employee_id=emp
 ########################################################
 # Business Logic
 ########################################################
-if experience >= 5:
-    bonus = salary * BONUS_EXPERIENCED / 100
-else:
-    bonus = salary * BONUS_NEW / 100
+    if experience >= 5:
+        bonus = salary * BONUS_EXPERIENCED / 100
+    else:
+        bonus = salary * BONUS_NEW / 100
 
-hra = salary * HRA_PERCENT / 100
+    hra = salary * HRA_PERCENT / 100
 
-total_salary = salary + bonus + hra
+    total_salary = salary + bonus + hra
 
-if experience >= 10:
-    status = "Senior Employee"
-else:
-    status = "Regular Employee"
+    if experience >= 10:
+        status = "Senior Employee"
+    else:
+        status = "Regular Employee"
 
-if total_salary > 200000:
-    income_category = "High Income Employee"
-else:
-    income_category = "Normal Income Employee"
+    if total_salary > 200000:
+        income_category = "High Income Employee"
+    else:
+        income_category = "Normal Income Employee"
 
 ########################################################
 # Report
 ########################################################
-print("----------------------------------------")
-print("EMPLOYEE REPORT")
-print("----------------------------------------")
+    print("-"*40)
+    print(f"EMPLOYEE REPORT {emp}")
+    print("-"*40)
 
-print(f'Employee ID     : {employee_id}')
-print(f'Employee Name   : {employee_name}')
-print(f'Department      : {department}')
-print(f'Experience      : {experience} Years')
-print(f'Basic Salary    : {salary}')
-print(f'Bonus           : ₹{bonus:,.2f}')
-print(f'HRA             : ₹{hra:,.2f}')
-print(f'Total Salary    : ₹{total_salary:,.2f}')
-print(f'Status          : {status}')
-print(f'Income Category : {income_category}')
-print("----------------------------------------")
+    print(f'Employee ID     : {employee_id}')
+    print(f'Employee Name   : {employee_name}')
+    print(f'Department      : {department}')
+    print(f'Experience      : {experience} Years')
+    print(f'Basic Salary    : {salary}')
+    print(f'Bonus           : ₹{bonus:,.2f}')
+    print(f'HRA             : ₹{hra:,.2f}')
+    print(f'Total Salary    : ₹{total_salary:,.2f}')
+    print(f'Status          : {status}')
+    print(f'Income Category : {income_category}')
+    print("-"*40)
---
+++ 06_loops/01_for_loop.py
@@ -0,0 +1,2 @@
+for number in range(5):
+    print(f'Processing employee no: {number+1}')
