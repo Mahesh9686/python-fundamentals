57c94f459cc17e06fd8714b5cf7f1c0acd2b6a8e
Mahesh <maheshvp055@gmail.com>
2026-07-13 14:40:46 +0530

    Add Dictionaries

---
+++ 08_dictionaries/01_dictionary_basics.py
@@ -0,0 +1,21 @@
+employee = {
+    "id" : 101,
+    "name" : "Mahesh",
+    "department" : "Data Engineering",
+    "salary" : 122000,
+    "experience" : 9
+}
+
+print(employee["salary"])
+
+print(employee["name"])
+
+print(employee["department"])
+
+employee["salary"] = 130000
+
+print(employee)
+
+employee["city"] = "Bangalore"
+
+print(employee)
